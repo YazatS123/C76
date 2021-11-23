89756e1451b44f102ee8eb6bd77f0a79069384c4
YazatS123 <77256681+YazatS123@users.noreply.github.com>
2021-11-22 21:13:59 -0600

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import IssTrackScreen from './screens/IssTrackScreen';
+import MeteorScreen from './screens/MeteorScreen';
+export default class App extends React.Component {
+  render(){
+  return (
+    <View>
+
+    </View>
+  );
+  }
+}
